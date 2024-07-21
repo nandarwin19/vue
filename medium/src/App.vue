8e8ee4d405c4e5d53971146373f99a977982a3d3
nwin19 <nandarwinbuma@gmail.com>
2024-07-21 22:40:14 +0630

    done 6. 4th Medium Project - Event Registration (Data Fetching)

--- medium/src/App.vue
+++ medium/src/App.vue
@@ -1,6 +1,60 @@
 <script setup>
 import EventCard from '@/components/EventCard.vue'
 import BookingItem from '@/components/BookingItem.vue'
+import LoadingEventCard from '@/components/LoadingEventCard.vue'
+import LoadingBookCard from '@/components/LoadingBookCard.vue'
+import { onMounted, ref } from 'vue'
+
+const events = ref([])
+const eventsLoading = ref(false)
+const books = ref([])
+const booksLoading = ref(false)
+
+// fetch events
+
+const fetchEvents = async () => {
+  eventsLoading.value = true
+  try {
+    const res = await fetch('http://localhost:3001/events')
+    events.value = await res.json()
+    console.log(events.value)
+  } finally {
+    eventsLoading.value = false
+  }
+}
+
+const fetchBooks = async () => {
+  booksLoading.value = true
+  try {
+    const res = await fetch('http://localhost:3001/bookings')
+    books.value = await res.json()
+    console.log(books.value)
+  } finally {
+    booksLoading.value = false
+  }
+}
+
+const handleRegister = async (event) => {
+  const newBooking = {
+    id: Date.now().toString(),
+    userId: 1,
+    eventId: event.id,
+    eventTitle: event.title
+  }
+
+  await fetch('http://localhost:3001/bookings', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      ...newBooking,
+      status: 'confirmed'
+    })
+  })
+}
+
+onMounted(() => {
+  fetchEvents(), fetchBooks()
+})
 </script>
 
 <template>
@@ -10,17 +64,29 @@ import BookingItem from '@/components/BookingItem.vue'
 
     <section class="grid grid-cols-2 gap-8">
       <!-- <EventCard v-for="i in 8" :key="i" /> -->
-      <EventCard
-        v-for="i in 8"
-        :key="i"
-        title="Vue Conference 2024"
-        when="2024-05-01"
-        description="Conference about Vue and JavaScript"
-      />
+      <template v-if="!eventsLoading">
+        <EventCard
+          v-for="event in events"
+          :key="event.id"
+          :title="event.title"
+          :when="event.date"
+          :description="event.description"
+          @register="handleRegister(event)"
+        />
+      </template>
+      <template v-else>
+        <LoadingEventCard v-for="i in 4" :key="i" />
+      </template>
     </section>
+
     <h2 class="text-2xl font-medium">Your Bookings</h2>
     <section class="grid grid-cols-1 gap-4">
-      <BookingItem v-for="i in 3" :key="i" />
+      <template v-if="!booksLoading">
+        <BookingItem v-for="book in books" :key="book.id" />
+      </template>
+      <template v-else>
+        <LoadingBookCard v-for="i in 3" :key="i" />
+      </template>
     </section>
   </main>
 </template>
---
+++ medium/src/components/LoadingBookCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <RoundedCard class="animate-pulse">
+    <div class="flex justify-between bg-gray-200">
+      <div class="p-4 border-b border-gray-200 h-8"></div>
+
+      <div class="h-8 w-24 bg-gray-300 rounded-full"></div>
+    </div>
+  </RoundedCard>
+</template>
+
+<script setup>
+import RoundedCard from '@/components/RoundedCard.vue'
+</script>
