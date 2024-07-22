53ba691f5884126d583f8a82e554ce1e22ffcba1
nwin19 <nandarwinbuma@gmail.com>
2024-07-22 12:19:44 +0630

    Refactor: Rename eventbooking

--- event-booking/src/App.vue
+++ event-booking/src/App.vue
@@ -17,7 +17,7 @@ const fetchEvents = async () => {
   try {
     const res = await fetch('http://localhost:3001/events')
     events.value = await res.json()
-    console.log(events.value)
+    // console.log(events.value)
   } finally {
     eventsLoading.value = false
   }
@@ -28,28 +28,66 @@ const fetchBooks = async () => {
   try {
     const res = await fetch('http://localhost:3001/bookings')
     books.value = await res.json()
-    console.log(books.value)
+    // console.log(books.value)
   } finally {
     booksLoading.value = false
   }
 }
 
+const findBookingById = (id) => books.value.findIndex((b) => b.id === id)
+
 const handleRegister = async (event) => {
+  if (books.value.find((book) => book.eventId === event.id))
+    return alert('You have already registered for this event')
+
   const newBooking = {
     id: Date.now().toString(),
     userId: 1,
     eventId: event.id,
-    eventTitle: event.title
+    eventTitle: event.title,
+    status: 'pending'
   }
 
-  await fetch('http://localhost:3001/bookings', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      ...newBooking,
-      status: 'confirmed'
+  books.value.push(newBooking)
+
+  try {
+    const res = await fetch('http://localhost:3001/bookings', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        ...newBooking,
+        status: 'confirmed'
+      })
     })
-  })
+
+    if (res.ok) {
+      const index = findBookingById(newBooking.id)
+      books.value[index] = await res.json()
+    } else {
+      throw new Error('Failed to confirm booking')
+    }
+  } catch (error) {
+    console.error(`Failed to register for event: `, error)
+    books.value = books.value.filter((book) => book.eventId !== newBooking.eventId)
+  }
+}
+
+const cancelBooking = async (bookingId) => {
+  const index = findBookingById(bookingId)
+  const originalBooking = books.value[index]
+  books.value.splice(index, 1)
+
+  try {
+    const res = await fetch(`http://localhost:3001/bookings/${bookingId}`, {
+      method: 'DELETE'
+    })
+    if (!res.ok) {
+      throw new Error('Booking could not be cancelled.')
+    }
+  } catch (e) {
+    console.error('Failed to cancel booking', e)
+    books.value.splice(index, 0, originalBooking)
+  }
 }
 
 onMounted(() => {
@@ -82,7 +120,13 @@ onMounted(() => {
     <h2 class="text-2xl font-medium">Your Bookings</h2>
     <section class="grid grid-cols-1 gap-4">
       <template v-if="!booksLoading">
-        <BookingItem v-for="book in books" :key="book.id" />
+        <BookingItem
+          v-for="book in books"
+          :key="book.id"
+          :status="book.status"
+          :title="book.eventTitle"
+          @cancelled="cancelBooking(BookingItem.id)"
+        />
       </template>
       <template v-else>
         <LoadingBookCard v-for="i in 3" :key="i" />
---
+++ event-booking/src/components/BookingItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <SectionCard>
+    <div class="flex justify-between">
+      <div class="flex space-x-2">
+        <div>{{ title }}</div>
+        <div>{{ status }}</div>
+      </div>
+      <RoundButton variant="danger" @click="$emit('cancelled')"> Cancel </RoundButton>
+    </div>
+  </SectionCard>
+</template>
+
+<script setup>
+defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  status: {
+    type: String,
+    required: true
+  }
+})
+
+defineEmits(['cancelled'])
+
+import SectionCard from '@/components/SectionCard.vue'
+import RoundButton from '@/components/RoundButton.vue'
+</script>
