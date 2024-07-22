7dc333d5f7819c40824496dfdf806ec86c93e607
nwin19 <nandarwinbuma@gmail.com>
2024-07-22 14:05:56 +0630

    errror-handling-and-recovery

--- event-booking/src/components/EventList.vue
+++ event-booking/src/components/EventList.vue
@@ -1,20 +1,34 @@
 <template>
-  <section class="grid grid-cols-2 gap-8">
-    <!-- <EventCard v-for="i in 8" :key="i" /> -->
-    <template v-if="!loading">
-      <EventCard
-        v-for="event in events"
-        :key="event.id"
-        :title="event.title"
-        :when="event.date"
-        :description="event.description"
-        @register="$emit('register', event)"
-      />
-    </template>
-    <template v-else>
-      <LoadingEventCard v-for="i in 4" :key="i" />
-    </template>
-  </section>
+  <template v-if="error">
+    <SectionCard>
+      <div class="space-y-4 items-center flex flex-col">
+        <div class="text-red-500">Could not load events at the moment. Please try again.</div>
+        <RoundButton @click="fetchEvents">Retry now</RoundButton>
+      </div>
+    </SectionCard>
+  </template>
+  <template v-else>
+    <section class="grid grid-cols-2 gap-8">
+      <template v-if="!loading">
+        <template v-if="events.length">
+          <EventCard
+            v-for="event in events"
+            :key="event.id"
+            :title="event.title"
+            :when="event.date"
+            :description="event.description"
+            @register="$emit('register', event)"
+          />
+        </template>
+        <template v-else>
+          <div class="col-span-2 text-center text-gray-500">No events yet.</div>
+        </template>
+      </template>
+      <template v-else>
+        <LoadingEventCard v-for="i in 4" :key="i" />
+      </template>
+    </section>
+  </template>
 </template>
 
 <script setup>
@@ -26,15 +40,19 @@ defineEmits(['register'])
 
 const events = ref([])
 const loading = ref(false)
+const error = ref(null)
 
 // fetch events
 
 const fetchEvents = async () => {
   loading.value = true
+  error.value = null
   try {
     const res = await fetch('http://localhost:3001/events')
     events.value = await res.json()
     // console.log(events.value)
+  } catch (e) {
+    error.value = e
   } finally {
     loading.value = false
   }
